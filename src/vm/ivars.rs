e88810466cc9363ce196d8ed710166fe018d5156
sisshiki1969 <sisshiki@mac.com>
2021-02-16 02:37:10 +0900

    Refactor.

--- src/vm/ivars.rs
+++ src/vm/ivars.rs
@@ -89,20 +89,17 @@ impl IvarCache {
             {
                 m.borrow_mut().accessor_all += 1;
             }
-            let slot = {
-                let entry = &mut m.borrow_mut().accessor[slot.into_usize()];
-                if let Some(iv_slot) = *entry {
-                    return iv_slot;
-                };
-                let iv_slot = ary.get_ivar_slot(name);
-                *entry = Some(iv_slot);
-                iv_slot
+            let entry = &mut m.borrow_mut().accessor[slot.into_usize()];
+            if let Some(iv_slot) = *entry {
+                return iv_slot;
             };
+            let iv_slot = ary.get_ivar_slot(name);
+            *entry = Some(iv_slot);
             #[cfg(feature = "perf-method")]
             {
                 m.borrow_mut().accessor_miss += 1;
             }
-            slot
+            iv_slot
         })
     }
 
@@ -123,23 +120,20 @@ impl IvarCache {
             {
                 m.borrow_mut().inline_all += 1;
             }
-            let slot = {
-                let entry = &mut m.borrow_mut().inline[slot.into_usize()];
-                if entry.class == Some(ary.class()) {
-                    return entry.iv_slot;
-                };
-                let iv_slot = ary.get_ivar_slot(name);
-                *entry = InlineIVCacheEntry {
-                    class: Some(ary.class()),
-                    iv_slot,
-                };
-                iv_slot
+            let entry = &mut m.borrow_mut().inline[slot.into_usize()];
+            if entry.class == Some(ary.class()) {
+                return entry.iv_slot;
+            };
+            let iv_slot = ary.get_ivar_slot(name);
+            *entry = InlineIVCacheEntry {
+                class: Some(ary.class()),
+                iv_slot,
             };
             #[cfg(feature = "perf-method")]
             {
                 m.borrow_mut().inline_miss += 1;
             }
-            slot
+            iv_slot
         })
     }
 }
